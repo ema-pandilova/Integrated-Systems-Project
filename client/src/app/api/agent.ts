799dbdaeac3d3c7f4fa14cf81ca4450ebdf3d571
emapandilova <ema.pandilova@yahoo.com>
2024-02-02 13:15:46 +0100

    Checkout feature implement

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -64,8 +64,16 @@ const requests = {
 const Account = {
     login: (values: any) => requests.post('account/login', values),
     register: (values: any) => requests.post('account/register', values),
-    currentUser: () => requests.get('account/currentUser')
+    currentUser: () => requests.get('account/currentUser'),
+    fetchAddress: () => requests.get('account/savedAddress')
 }
+
+const Orders = {
+    list: () => requests.get('orders'),
+    fetch: (id: number) => requests.get(`orders/${id}`),
+    create: (values: any) => requests.post('orders', values)
+}
+
 
 const Basket = {
     get: () => requests.get('basket'),
@@ -91,7 +99,8 @@ const Catalog = {
         Catalog,
         TestErrors,
         Basket,
-        Account
+        Account,
+        Orders
  }
 
  export default agent;
